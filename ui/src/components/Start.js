5a88c6201efd29aa3100620aac0e1229562c4ac5
bipni <bipumirja@gmail.com>
2022-08-15 23:43:49 +0600

    registration added

--- ui/src/components/Start.js
+++ ui/src/components/Start.js
@@ -3,15 +3,33 @@ import '../styles/Start.css'
 function Start() {
 
     return (
-        <div className="login">
-            <h1>Group Organizer</h1>
+        <div>
+            <div className="login">
+                <h1>Group Organizer</h1>
 
-            <div className="login-area">
-                <form>
-                    <label>Email: <input type="text" name="email"/></label>
-                    <label>Password: <input type="password" name="password"/></label>
-                    <input type="submit"/>
-                </form>
+                <div className="login-area">
+                    <form>
+                        <label>Email: <input type="text" name="email"/></label>
+                        <label>Password: <input type="password" name="password"/></label>
+                        <input type="submit" value="Submit"/>
+                    </form>
+                </div>
+            </div>
+
+            <div className="registration">
+                <h3>Registration</h3>
+
+                <div className="registration-area">
+                    <form>
+                        <label className="reg-lab-in">First Name</label>
+                        <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+
+                        <label className="reg-lab-in">Last Name</label>
+                        <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+  
+                        <input type="submit" value="Submit"/>
+                    </form>
+                </div>
             </div>
         </div>
     );
